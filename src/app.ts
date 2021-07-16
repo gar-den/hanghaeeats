029c04ac2c1cefdbf240386901b642eb7de726b2
ohbin kwon <ohbinkwon.dev@gmail.com>
2021-07-16 19:44:37 +0900

    [오빈]  made bone files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
-// import { cartRouter } from "./routers/cart";
-// import { menuRouter } from "./routers/menu";
-// import { orderRouter } from "./routers/order";
-// import { reviewRouter } from "./routers/review";
-// import { shopRouter } from "./routers/shop";
-// import { userRouter } from "./routers/user";
-import { connect } from './schemas'
+import { cartRouter } from './routers/cart';
+import { menuRouter } from './routers/menu';
+import { orderRouter } from './routers/order';
+import { reviewRouter } from './routers/review';
+import { shopRouter } from './routers/shop';
+import { userRouter } from './routers/user';
+import { connect } from './schemas';
 
 const app = express(); // variable automatically inferred as Application
 app.use(express.urlencoded({ extended: false }));
@@ -16,13 +16,13 @@ const port = 5000;
 connect();
 
 // APIs
-// app.use('/api/user', [userRouter]);
-// app.use('/api/cart', [cartRouter]);
-// app.use('/api/menu', [menuRouter]);
-// app.use('/api/review', [reviewRouter]);
-// app.use('/api/order', [orderRouter]);
-// app.use('/api/shop', [shopRouter]);
+app.use('/api/user', [userRouter]);
+app.use('/api/cart', [cartRouter]);
+app.use('/api/menu', [menuRouter]);
+app.use('/api/review', [reviewRouter]);
+app.use('/api/order', [orderRouter]);
+app.use('/api/shop', [shopRouter]);
 
 app.listen(5000, () => {
-    console.log("listening at http://localhost:5000");
-})
+  console.log('listening at http://localhost:5000');
+});
