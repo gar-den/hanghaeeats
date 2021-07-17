28f746476a82e91aaa571c778b8e7767777858d0
Garden <prepella3@gmail.com>
2021-07-17 14:37:11 +0900

    [garden] doing orders

--- src/routers/order.ts
+++ src/routers/order.ts
@@ -1,10 +1,39 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
-import Orders from '../schemas/cart';
+import { authMiddleware } from '../auth-middleware';
+import Orders from '../schemas/order';
+import Menus from '../schemas/menu';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/', async (req, res) => {
-  res.json({ message: 'success' });
+orderRouter.post('/', authMiddleware, async (req, res) => {
+  const { storeId, menus } = req.body;
+
+  const userId = res.locals.user._id
+  const today = new Date();
+  let price = 0;
+  let menu_price = 0;
+  let menu;
+
+  for (let i = 0; i < menus.length; i ++) {
+    menu = await Menus.findOne({ _id: menus[i].menuId });
+    menu_price = menu.price;
+
+    price += (menu_price * menus[i].count);
+  }
+
+  await Orders.create({ storeId, menus, userId, orderDate: today, price });
+  
+  const order = await Orders.findOne({ orderDate: today });
+
+  res.json({ message: 'success', orderId: order._id });
 });
+
+orderRouter.get('/', authMiddleware, async (req, res) => {
+  const userId = res.locals.user._id;
+
+  const order = await Orders.find({ userId });
+
+  res.json({ message: 'success', order });
+})
 
 export { orderRouter };
--- src/routers/store.ts
+++ src/routers/store.ts
@@ -3,47 +3,22 @@ import Stores from '../schemas/store'
 
 const storeRouter = express.Router();
 
-//가게 전부 가져오기 (query값 입력하지 않으면 됨)
-// 정해진 카테고리에 맞는 가게 전부 가져오기
-storeRouter.get('/', async (req, res) => {
-    const { category } = req.query
-    try{        
-        const categorizedStores = await Stores.find({ category : category }).exec()
-        res.status(200).json({
-            message : "success",
-            stores : categorizedStores,
-        }) 
-    } catch (err) {
-        res.status(400).json({
-          message: 'fail',
-          err: err,
-        });
-    }
-    
-})
+storeRouter.get('/:pageNo', async (req, res) => {
+  const pageNo = Number(req.params.pageNo);
+  const stores = await Stores.find({});
+  const stores_page:Array<object> = [];
 
-storeRouter.get('/', async (req, res) => {
-  const searchWord = req.query.q
-  console.log(req.query)
-  try{
-      const searchedStores = await Stores.find({ name : searchWord }).exec()
+  if (pageNo * 10 >= stores.length || pageNo < 0) {
+    res.json({ message: "fail" });
 
-      if (!searchedStores){
-        return res.status(400).json({
-          message : "해당 정보가 존재하지 않습니다."
-        })
-      } 
-      res.status(200).json({
-          message : "success",
-          stores : searchedStores,
-      }) 
-  } catch (err) {
-      res.status(400).json({
-        message: 'fail',
-        err: err,
-      });
+    return;
   }
-  
+
+  for (let i: number = pageNo * 10; i < pageNo * 10 + 10; i ++) {
+    stores_page.push(stores[i]);
+  }
+
+  res.json({ message: "success", stores: stores_page });
 })
 
     
