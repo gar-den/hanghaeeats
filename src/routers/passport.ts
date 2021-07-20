3f42c715386068f6fe1862855c45a2b0348eb06e
Garden <prepella3@gmail.com>
2021-07-20 21:02:05 +0900

    [garden] google auth success!

--- src/routers/passport.ts
+++ src/routers/passport.ts
@@ -16,15 +16,28 @@ const passportConfig = () => {
         clientID: "498392378371-j0sjs2lf9siqjk543e3m4klvjshhe7b6.apps.googleusercontent.com",
         clientSecret: "lBHWEr64K-5R0O66m9GfR9JK",
         callbackURL: "http://localhost:5000/api/user/google/callback"
+        // callbackURL: "http://nameisgarden.xyz/api/user/google/callback"
       },
       async function(accessToken, refreshToken, profile, done:GoogleStrategy.VerifyCallback) {
         const userId = profile.id;
         const email = profile.email;
         const nickname = profile.name.givenName;
         const provider = profile.provider
-        const user = await Users.findOne({ profile: [{provider: provider}, {id: userId}] });
 
-        const token = jwt.sign({ userId: user._id }, "gardenisthebest");
+        let user: any;
+
+        user = await Users.findOne({profile: [{provider: "google"}, {id: userId}]})
+
+        if (!user) {
+            await Users.create({ profile: [{provider: provider}, {id: userId}], email, nickname });
+
+            user = await Users.findOne({profile: [{provider: "google"}, {id: userId}]})
+        }
+
+        const _id = user._id;
+
+        const token = jwt.sign({ userId: _id }, "gardenisthebest");
+
         return done(null, profile, { message: token });
       }
     ));
