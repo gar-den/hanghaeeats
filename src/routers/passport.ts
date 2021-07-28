2009ec95600b29064fa6b142df8f527420338463
Garden <62330320+gar-den@users.noreply.github.com>
2021-07-28 17:37:43 +0900

    Update passport.ts

--- src/routers/passport.ts
+++ src/routers/passport.ts
@@ -14,8 +14,7 @@ const passportConfig = () => {
     
     passportRouter.use(new Strategy({
         clientID: "498392378371-j0sjs2lf9siqjk543e3m4klvjshhe7b6.apps.googleusercontent.com",
-        clientSecret: "lBHWEr64K-5R0O66m9GfR9JK",
-        // callbackURL: "http://localhost:5000/api/user/google/callback"
+        clientSecret: "",
         callbackURL: "http://nameisgarden.xyz/api/user/google/callback"
       },
       async function(accessToken, refreshToken, profile, done:GoogleStrategy.VerifyCallback) {
